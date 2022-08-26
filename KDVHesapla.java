8d19e38951dab6de614420f979f64869cb6f7847
DCanKayrak <94143272+DCanKayrak@users.noreply.github.com>
2022-08-26 23:27:41 +0300

    Create KDVHesapla.java

---
+++ KDVHesapla.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class NotHesapla {
+    public static void main(String[] args) {
+        Scanner gir = new Scanner(System.in);
+        float kdvsizFiyat,kdvliFiyat,kdvOrani;
+
+        System.out.println("KDVsiz fiyatı giriniz : ");
+        kdvsizFiyat = gir.nextFloat();
+
+        if(kdvsizFiyat>0 && kdvsizFiyat<1000){
+            kdvOrani = 0.18f;
+        }else{
+            kdvOrani = 0.08f;
+        }
+
+        kdvliFiyat = kdvsizFiyat + (kdvsizFiyat*kdvOrani);
+        System.out.println(kdvliFiyat);
+
+    }
+}
