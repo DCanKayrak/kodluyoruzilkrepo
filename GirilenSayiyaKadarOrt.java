adcc8bf165ccbb51e45839f1dcd9c1e2eb00e25f
DCanKayrak <94143272+DCanKayrak@users.noreply.github.com>
2022-08-27 14:18:44 +0300

    Create GirilenSayiyaKadarOrt.java

---
+++ GirilenSayiyaKadarOrt.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class GirilenSayiyaKadarCift {
+    public static void main(String[] args) {
+        Scanner gir = new Scanner(System.in);
+
+        System.out.println("Sayi giriniz : ");
+        int sayi = gir.nextInt();
+        int ortalama = 0;
+        for(int i=1;i<sayi;i++){
+            if(i%3 == 0 && i%4 == 0){
+                ortalama += i;
+            }
+        }
+        System.out.println(ortalama);
+    }
+}
